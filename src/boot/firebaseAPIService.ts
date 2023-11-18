22265ac999411b56f6b92f2da6af428a178c4de0
Meliodas <banktv76@gmail.com>
2023-11-18 11:38:17 +0700

    Update.

--- src/boot/firebaseAPIService.ts
+++ src/boot/firebaseAPIService.ts
@@ -5,18 +5,18 @@ const createTimestamp = (): Date => {
 
 // CREATE Function.
 
-export const createContent = async (userId: number, newContent: string) => {
-  // Create function to create Content To firebase.
-};
+// export const createContent = async (userId: number, newContent: string) => {
+//   // Create function to create Content To firebase.
+// };
 
 // READ Function.
 
-export const readAllContent = async (): Promise<Content[]> => {
-  // Create function to readAllContent from firebase.
-};
+//   export const readAllContent = async (): Promise<Content[]> => {
+//     // Create function to readAllContent from firebase.
+// };
 
 // DELETE Function.
 
-export const deleteContent = async (contentId: string) => {
-  // Create function to delete Content in firebase.
-};
+// export const deleteContent = async (contentId: string) => {
+//   // Create function to delete Content in firebase.
+// };
